3bdf00b4747ab361f0c4cfd836c8d7e66abad668
ARI VPN STORES <145305412+arivpnstores@users.noreply.github.com>
2025-11-08 04:51:34 +0000

    Update reinstall2.sh

--- reinstall2.sh
+++ reinstall2.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh -O reinstall.sh && chmod +x reinstall.sh && bash reinstall.sh ubuntu 20.04
+wget https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh -O reinstall && chmod +x reinstall && bash reinstall ubuntu 20.04
